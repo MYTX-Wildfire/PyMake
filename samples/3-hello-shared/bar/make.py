e9ab58cc0c74e56aba840b630335a8ef5f5516e4
Zach Wilson <zach.wilson@wizard.dev>
2023-03-13 23:48:45 +0000

    Get sample 3 working again

--- samples/3-hello-shared/bar/make.py
+++ samples/3-hello-shared/bar/make.py
@@ -1,9 +1,12 @@
+from pymake import EScope
 from foo.make import foo_target
 from make import project
 
+# Create the target set for the library
+target_set = project.create_target_set("BarTargetSet")
+
 # Configure the Bar executable
-bar_target = project.add_executable("bar")
-bar_target.link_to_target(foo_target)
-bar_target.add_sources("bar.cpp")
-bar_target.set_install_rpath("$ORIGIN/../lib")
+bar_target = target_set.add_executable("bar")
+bar_target.link_to(EScope.PRIVATE, foo_target)
+bar_target.add_sources(EScope.PRIVATE, "bar.cpp")
 bar_target.install()
